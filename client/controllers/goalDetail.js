88474bd2542b106bb38dd509634b268f2eab6eb6
Tenari <djz24@case.edu>
2014-07-29 13:49:55 -0400

    goalDetail closer. Social plugins getting replaced by custom API connects

--- client/controllers/goalDetail.js
+++ client/controllers/goalDetail.js
@@ -18,12 +18,41 @@ Template.goalDetail.helpers({
     return parseInt((Date.now() - this.created_at)/(1000*60*60*24));
   },
   donation : function(){
-    return Donations.find({goalId: this._id});
+    return Donations.find({goalId: this._id}, {skip: Session.get('skip') || 0, limit: 10});
   },
   donationCount : function(){
     return Donations.find({goalId: this._id}).count();
   },
   amountF : function(){
     return parseFloat(this.amount).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,').slice(0, -3);
+  },
+  formatCreatedAt : function(){
+    return (new Date(this.created_at)).toISOString();
+  },
+  donationShowingText : function(){
+    var count = Donations.find({goalId: this._id}).count();
+    return "<span class='bold'>"+(Session.get('skip') || 1)+" - "+Math.min((Session.get('skip') || 0)+10, count)+"</span> of <span class='bold'>"+count+"</span>";
+  },
+  previousDonations : function(){
+    return (Session.get('skip') || 0) > 0;
+  },
+  nextDonations : function(){
+    return (Session.get('skip') || 0)+10 < Donations.find({goalId: this._id}).count();
   }
 });
+
+Template.goalDetail.events({
+  'click .next-donations':function(e){
+    var count = Donations.find({goalId: this._id}).count();
+    Session.set('skip', Math.min((Session.get('skip') || 0) + 10, Math.max(0, count - 10)));
+    setTimeout(function(){jQuery("time.timeago").timeago();}, 1000);
+  },
+  'click .prev-donations':function(e){
+    Session.set('skip', Math.max((Session.get('skip') || 0) - 10, 0));
+    setTimeout(function(){jQuery("time.timeago").timeago();}, 1000);
+  }
+});
+
+Template.goalDetail.rendered = function(){
+  setTimeout(function(){jQuery("time.timeago").timeago();}, 1000);
+};
