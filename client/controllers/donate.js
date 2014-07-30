bb94015d9b53dbf7f6d2863ef6a3057ea031e7ad
Tenari <djz24@case.edu>
2014-07-30 13:27:35 -0400

    payment methods are created by donation page

--- client/controllers/donate.js
+++ client/controllers/donate.js
@@ -1,11 +1,38 @@
 Template.donate.events({
-  'click button': function(){
-    Donations.insert({
-      amount: $('.amount').val(),
-      goalId: this._id,
-      name: $('.donate-name').val().trim(),
-      msg: $('.donate-msg').val().trim(),
-      created_at: Date.now()
-    });
+  'click .submit-btn': function(e){
+    var amt = $('.amount').val();
+    if (amt == "") {
+      e.preventDefault();
+    } else {
+    }
   }
 });
+
+Template.donate.rendered = function(){
+  Meteor.call('clientToken', function(err, res){
+    if( err) return;
+    $('#dropin').text(""); // clear the 'loading form...'
+    braintree.setup(res.result, 'dropin', {
+      container: 'dropin',
+      paymentMethodNonceReceived: function (event, nonce) {
+        var name = $('.donate-name').val().trim();
+        Meteor.call('donate', nonce, name, function(err,res){
+          console.log(err);
+          console.log(res);
+          Donations.insert({
+            amount: $('.amount').val(),
+            goalId: $('#goal-id').text(),
+            name: name,
+            msg: $('.donate-msg').val().trim(),
+            created_at: Date.now(),
+            submitted: false,
+            customer: res.result.customer.id
+          }, function(err, id){
+            alert('Your donation has been processed. You will be charged only if the goal owner indicates that they completed the goal.');
+            window.location.pathname = '/goal/' + $('#goal-id').text();
+          });
+        });
+      }
+    });
+  });
+};
--- server/methods.js
+++ server/methods.js
@@ -1,5 +1,37 @@
+var bt = Meteor.require('braintree');
+var gateway = bt.connect({
+  environment: bt.Environment.Sandbox,
+  merchantId: 'vfhpwqw9g896qnzh',
+  publicKey: 'p8s4g5cpczwqj3gp',
+  privateKey: '97f4b0a67bf1974bea764beccd95f8f4'
+});
+var clientToken = "";
 Meteor.methods({
   twitter_share_count : function(url) {
     return HTTP.get('http://urls.api.twitter.com/1/urls/count.json?url='+url);
+  },
+  donate : function(nonce, name){
+    if(name == "") name = "Anonymous";
+    var result = Async.runSync(function(done){
+      gateway.customer.create({
+        firstName: name,
+        paymentMethodNonce: nonce
+      }, function(err, data){
+        done(null, data);
+      });
+    });
+    return result;
+  },
+  clientToken : function(){
+    if(clientToken == ""){
+      clientToken = Async.runSync(function(done){
+        gateway.clientToken.generate({
+        //  customerId: aCustomerId
+        }, function (err, response) {
+          done(err, response.clientToken);
+        });
+      });
+    }
+    return clientToken;
   }
 });
